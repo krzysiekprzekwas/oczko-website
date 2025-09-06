ee4c0479c72e4a557f5faa1f92aad21920fda18e
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2025-09-06 13:30:42 +0200

    Contact section

---
+++ app/contact/ContactSection.tsx
@@ -0,0 +1,52 @@
+"use client";
+import * as React from "react";
+import Link from "../components/Link";
+
+export function ContactSection() {
+  return (
+    <>
+      <section id="cofounders" className="flex flex-col md:flex-row w-full md:aspect-[2/1]">
+        <article className="w-full md:w-1/2 md:h-full md:aspect-square flex flex-col justify-center items-center bg-oczko-green-300 border-[0.5px] border-neutral-800">
+            <div className="flex flex-col flex-1 shrink gap-6 items-center self-stretch w-full basis-0 md:min-h-[600px] min-w-60 max-md:max-w-full px-10 py-16 max-md:px-4 max-md:py-6">
+              <div className="gap-12 self-stretch w-full max-md:max-w-full">
+                <h1 className="text-7xl leading-tight text-oczko-violet-500 font-salted max-md:max-w-full font-salted">
+                  Kontakt
+                </h1>
+                <p className="mt-4 text-base font-medium leading-5 text-oczko-violet-500 max-md:max-w-full">
+                  Masz do nas pytanie? A może chcesz dowiedzieć się więcej o ofercie?
+                </p>
+                <p className="mt-4 text-base font-medium leading-5 text-oczko-violet-500 max-md:max-w-full">
+                  Napisz do nas na oczko.workshops@gmail.com a my odpowiemy w mgnieniu OCZKA!
+                </p>
+                <h3 className="text-5xl leading-tight text-oczko-violet-500 font-salted max-md:max-w-full max-md:text-2xl font-salted">
+                  Social Media
+                </h3>
+                <p className="mt-4 text-base font-medium leading-5 text-oczko-violet-500 max-md:max-w-full">
+                  Rzuć OCZKIEM na nasze sociale i bądź na bieżąco!
+                </p>
+              </div>
+              <div className="flex gap-4 w-full justify-start">
+                <Link
+                  href="https://www.instagram.com/oczko.workshops/"
+                  text="Instagram"
+                  color="oczko-violet-500"
+                />
+                <Link
+                  href="https://www.facebook.com/profile.php?id=61567128943041"
+                  text="Facebook"
+                  color="oczko-violet-500"
+                />
+              </div>
+            </div>
+        </article>
+        <div className="w-full aspect-square md:w-1/2 md:h-full md:aspect-square flex justify-center items-center bg-oczko-violet-100 border-[0.5px] border-neutral-800">
+            <img
+              src="/images/contact.png"
+              className="object-contain w-full"
+              alt="Contact"
+            />
+        </div>
+      </section>
+    </>
+  );
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,14 +3,15 @@ import { Footer } from "../components/Footer";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Banner } from "../components/Banner";
-import { PhotoCarousel } from "../components/PhotoCarousel";
 import { CTASection } from "../components/CTASection";
+import { ContactSection } from "./ContactSection";
 
 export default function Contact() {
   return (
     <main>
       <Navigation />
-      Kontakt
+      <ContactSection />
+      Partners Banner
       <CTASection />
       <Banner />
       <Footer />
