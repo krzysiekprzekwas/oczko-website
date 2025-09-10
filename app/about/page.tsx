1cdc31bc0fb38627659a9fef4f185f19664a82f2
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2025-09-10 07:35:14 +0200

    About us images

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,29 @@
 import { Footer } from "../components/Footer";
 import { Banner } from "../components/Banner";
-import { PhotoCarousel } from "../components/PhotoCarousel";
+import { CarouselImage, PhotoCarousel } from "../components/PhotoCarousel";
 import { CofoundersSection } from "./CofoundersSection";
+
+const aboutImages: CarouselImage[] = [
+  {
+    src: "/images/about_carousel_1.png",
+    alt: "O nas photo 1",
+  },
+  {
+    src: "/images/about_carousel_2.png",
+    alt: "O nas photo 2",
+  },
+  {
+    src: "/images/about_carousel_3.png",
+    alt: "O nas photo 3",
+  },
+];
 
 export default function About() {
   return (
     <>
       <CofoundersSection />
       <Banner />
-      <PhotoCarousel />
+      <PhotoCarousel images={aboutImages}/>
       <Footer />
     </>
   );
