da4394d3a04417fa81cf66a538cb304779aa9398
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2025-09-10 07:41:11 +0200

    Workshops photos

--- app/workshops/page.tsx
+++ app/workshops/page.tsx
@@ -2,10 +2,26 @@ import { Footer } from "../components/Footer";
 import { Banner } from "../components/Banner";
 import { NoodleWorkshopsSection } from "./NoodleWorkshopsSection";
 import { BannerPartners } from "../components/BannerPartners";
-import { PhotoCarousel } from "../components/PhotoCarousel";
+import { CarouselImage, PhotoCarousel } from "../components/PhotoCarousel";
 import { WorkshopActivitiesSection } from "./WorkshopActivitiesSection";
 import { B2BSection } from "./B2BSection";
 import { AccessoriesWorkshopsSection } from "./AccessoriesWorkshopsSection";
+
+const workshopsImages: CarouselImage[] = [
+  {
+    src: "/images/workshops_carousel_1.png",
+    alt: "Workshops photo 1",
+  },
+  {
+    src: "/images/workshops_carousel_2.png",
+    alt: "Workshops photo 2",
+  },
+  {
+    src: "/images/workshops_carousel_3.png",
+    alt: "Workshops photo 3",
+  },
+];
+
 
 export default function Workshops() {
   return (
@@ -14,7 +30,7 @@ export default function Workshops() {
       <Banner />
       <AccessoriesWorkshopsSection />
       <BannerPartners />
-      <PhotoCarousel />
+      <PhotoCarousel images={workshopsImages}/>
       <WorkshopActivitiesSection />
       <B2BSection/>
       <Footer backgroundColor="bg-oczko-orange-400" color="oczko-green-1000"/>
