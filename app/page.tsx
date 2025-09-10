436b0e24453af41c17a267222faff21d71f967a7
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2025-09-10 07:27:15 +0200

    Update images

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       <WorkshopsSection />
       <section className="max-md:hidden">
         <img
-        src="https://cdn.builder.io/api/v1/image/assets/bcf9f0d5781a42d0990f584b23abfb2f/bfa5bee6d4f8cb3de63a21bdf8d206877ba3137d?placeholderIfAbsent=true"
+          src="./images/group.png"
           className="object-contain w-full max-md:max-w-full"
           alt="Workshop scene"
         />
